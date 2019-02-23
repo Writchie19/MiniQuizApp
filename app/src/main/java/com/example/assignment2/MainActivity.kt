46acc0d36dcda5c8047b7f829ce875bde7a8e8b5
William Ritchie <will.ritchie@yahoo.com>
2019-02-22 16:02:30 -0800

    UserInformation Class, read/write file

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -25,31 +25,19 @@ class MainActivity : AppCompatActivity() , OnEditorActionListener{
         var handled = false
         if (actionId == EditorInfo.IME_ACTION_DONE) {
             handled = true
-            Log.i("FIRSTNAME", findViewById<EditText>(R.id.First_Name_Edit).text.toString())
-            Log.i("LASTNAME", findViewById<EditText>(R.id.Last_Name_Edit).text.toString())
-            Log.i("NICKNAME", findViewById<EditText>(R.id.Nick_Name_Edit).text.toString())
-            Log.i("AGE", findViewById<EditText>(R.id.Age_Edit).text.toString())
+            val newFirstName = findViewById<EditText>(R.id.First_Name_Edit).text.toString()
+            val newLastName = findViewById<EditText>(R.id.Last_Name_Edit).text.toString()
+            val newNickName = findViewById<EditText>(R.id.Nick_Name_Edit).text.toString()
+            val newAge = findViewById<EditText>(R.id.Age_Edit).text.toString()
             val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             manager.hideSoftInputFromWindow(v?.windowToken, 0)
-            writeToFile()
-            readFromFile()
+            val userInfo = UserInformation(this)
+            userInfo.update(newFirstName, newLastName, newNickName, newAge)
+            Log.i("FIRSTNAME", userInfo.getFirstName())
+            Log.i("LASTNAME", userInfo.getLastName())
+            Log.i("NICKNAME", userInfo.getNickName())
+            Log.i("AGE", userInfo.getAge())
         }
         return handled
-    }
-
-    fun writeToFile(){
-        val outPutFile = File(this.filesDir, "UsrInfo")
-        val jsonObj = JSONObject()
-        jsonObj.put("First_Name", findViewById<EditText>(R.id.First_Name_Edit).text.toString())
-        jsonObj.put("Last_Name", findViewById<EditText>(R.id.Last_Name_Edit).text.toString())
-        jsonObj.put("Nick_Name", findViewById<EditText>(R.id.Nick_Name_Edit).text.toString())
-        jsonObj.put("Age", findViewById<EditText>(R.id.Age_Edit).text.toString())
-        outPutFile.writeText(jsonObj.toString())
-    }
-
-    fun readFromFile() {
-        val inputFile = File(this.filesDir, "UsrInfo")
-        Log.i("DIR", this.filesDir.toString())
-        Log.i("UsrInfo",inputFile.readText())
     }
 }
---
+++ app/src/main/java/com/example/assignment2/UserInformation.kt
@@ -0,0 +1,85 @@
+package com.example.assignment2
+
+import android.content.Context
+import android.util.Log
+import org.json.JSONObject
+import java.io.File
+
+private const val USERFILENAME = "UserInfo"
+private const val FIRSTNAMETAG = "First_Name"
+private const val LASTNAMETAG = "Last_Name"
+private const val NICKNAMETAG = "Nick_Name"
+private const val AGETAG = "Age"
+
+class UserInformation {
+    private val userFile : File
+    private var userInfo : JSONObject
+    private var firstName : String
+    private var lastName : String
+    private var nickName : String
+    private var age : String
+
+    constructor(context : Context) {
+        userFile = File(context.filesDir, USERFILENAME)
+        userFile.createNewFile()
+        Log.i("MEG", "HERE?")
+        if (userFile.readBytes().isNotEmpty()) {
+            // what if readText is not in JSON format?
+            Log.i("meg", "DID I MAKE IT HERE?")
+            userInfo = JSONObject(userFile.readText())
+            firstName = userInfo.get(FIRSTNAMETAG).toString()
+            lastName = userInfo.get(LASTNAMETAG).toString()
+            nickName = userInfo.get(NICKNAMETAG).toString()
+            age = userInfo.get(AGETAG).toString()
+        }
+        else {
+            Log.i("meg", "ELSE")
+            userInfo = JSONObject()
+            firstName = ""
+            lastName = ""
+            nickName = ""
+            age = ""
+            userInfo.put(FIRSTNAMETAG, firstName)
+            userInfo.put(LASTNAMETAG, lastName)
+            userInfo.put(NICKNAMETAG, nickName)
+            userInfo.put(AGETAG, age)
+            userFile.writeText(userInfo.toString())
+        }
+    }
+
+    fun readFromFile() {
+        userInfo = JSONObject(userFile.readText())
+        firstName = userInfo.get(FIRSTNAMETAG).toString()
+        lastName = userInfo.get(LASTNAMETAG).toString()
+        nickName = userInfo.get(NICKNAMETAG).toString()
+        age = userInfo.get(AGETAG).toString()
+    }
+
+    fun update(newFirstName : String, newLastName : String, newNickName : String, newAge : String ) {
+        firstName = newFirstName
+        lastName = newLastName
+        nickName = newNickName
+        age = newAge
+        userInfo.put(FIRSTNAMETAG, firstName)
+        userInfo.put(LASTNAMETAG, lastName)
+        userInfo.put(NICKNAMETAG, nickName)
+        userInfo.put(AGETAG, age)
+        userFile.writeText(userInfo.toString())
+    }
+
+    fun getFirstName() : String{
+        return firstName
+    }
+
+    fun getLastName() : String{
+        return lastName
+    }
+
+    fun getNickName() : String{
+        return nickName
+    }
+
+    fun getAge() : String{
+        return age
+    }
+}
