80e98f2b474b613f4dfbb27ceb43df2d750974ab
William Ritchie <will.ritchie@yahoo.com>
2019-02-22 21:14:15 -0800

    Activity Cross Communication

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -1,29 +1,32 @@
 package com.example.assignment2
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
-import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import android.widget.TextView.OnEditorActionListener
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.File
-import org.json.JSONObject
+
+private const val QUIZREQUESTCODE = 12345
+
 class MainActivity : AppCompatActivity() , OnEditorActionListener{
+    private lateinit var userInfo : UserInformation
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Age_Edit.setOnEditorActionListener(this)
+        userInfo = UserInformation(this)
         Quiz_Button.setOnClickListener{
+            updateUserInformation()
             val quizIntent = Intent(this, QuizActivity::class.java)
-            startActivity(quizIntent)
+            startActivityForResult(quizIntent, QUIZREQUESTCODE)
         }
     }
 
@@ -31,15 +34,34 @@ class MainActivity : AppCompatActivity() , OnEditorActionListener{
         var handled = false
         if (actionId == EditorInfo.IME_ACTION_DONE) {
             handled = true
-            val newFirstName = First_Name_Edit.text.toString()
-            val newLastName = Last_Name_Edit.text.toString()
-            val newNickName = Nick_Name_Edit.text.toString()
-            val newAge = Age_Edit.text.toString()
             val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             manager.hideSoftInputFromWindow(v?.windowToken, 0)
-            val userInfo = UserInformation(this)
-            userInfo.update(newFirstName, newLastName, newNickName, newAge)
+            updateUserInformation()
         }
         return handled
+    }
+
+    private fun updateUserInformation(){
+        val newFirstName = First_Name_Edit.text.toString()
+        val newLastName = Last_Name_Edit.text.toString()
+        val newNickName = Nick_Name_Edit.text.toString()
+        val newAge = Age_Edit.text.toString()
+        userInfo.update(newFirstName, newLastName, newNickName, newAge)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.i("QUIZ", "HERE?")
+        if (requestCode != QUIZREQUESTCODE) {
+            Log.i("QUIZ", "Failed")
+            return
+        }
+        when (resultCode) {
+            Activity.RESULT_OK -> {
+                val quizResult = data?.getIntExtra("result", -1)
+                Log.i("QUIZ", "Quiz Result: " + quizResult.toString())
+            }
+            Activity.RESULT_CANCELED ->
+                Log.i("QUIZ", "cancel")
+        }
     }
 }
--- app/src/main/java/com/example/assignment2/QuizActivity.kt
+++ app/src/main/java/com/example/assignment2/QuizActivity.kt
@@ -1,5 +1,6 @@
 package com.example.assignment2
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_quiz.*
@@ -9,6 +10,11 @@ class QuizActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
-        Back_Button.setOnClickListener{finish()}
+        Back_Button.setOnClickListener{
+            val quizResultToPassBack = intent
+            quizResultToPassBack.putExtra("result", 50)
+            setResult(Activity.RESULT_CANCELED, quizResultToPassBack)
+            finish()
+        }
     }
 }
